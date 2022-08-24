b20dfe60a7dd9c3824a432bd5703ee465b7dfc1c
reazmoj <reazmoj.h@gmail.com>
2022-08-24 23:06:52 +0430

    files added

---
+++ ex/process.php
@@ -0,0 +1,22 @@
+<?php
+
+$errors = [];
+$data = [];
+
+if (empty($_POST['name'])) {
+    $errors['name'] = 'Name is required.';
+}
+
+if (empty($_POST['email'])) {
+    $errors['email'] = 'Email is required.';
+}
+
+if (!empty($errors)) {
+    $data['success'] = false;
+    $data['errors'] = $errors;
+} else {
+    $data['success'] = true;
+    $data['message'] = 'Success!';
+}
+
+echo json_encode($data);
